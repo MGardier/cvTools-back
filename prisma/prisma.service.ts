3021b52b298dfa0dbed4233d717133bfe2a057cb
Maxime Gardier <m.gardierpro@gmail.com>
2026-09-17 15:27:14 +0200

    chore: Upgrade Prisma

--- prisma/prisma.service.ts
+++ prisma/prisma.service.ts
@@ -1,11 +1,13 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from './generated/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
     constructor() {
     const showLog = process.env.SHOW_SQL === 'true';
     super({
+      adapter: new PrismaPg({ connectionString: process.env.DATABASE_URL }),
       log: showLog ?
       [
         {
